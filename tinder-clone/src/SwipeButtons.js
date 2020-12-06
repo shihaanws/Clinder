c5f186e8214260aa6aa56015fbe944ce920dd547
Shihaan <42083808+theneoterik@users.noreply.github.com>
2020-12-07 01:03:11 +0530

    Update SwipeButtons.js

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import CloseIcon from '@material-ui/icons/Close';
-import FavoriteIcon from '@material-ui/icons/Favorite';
 import Button from "@material-ui/core/Button";
 import CallIcon from '@material-ui/icons/Call';
 import "./SwipeButtons.css";
@@ -13,7 +12,7 @@ const StyledButton = withStyles({
       border: 0,
       color: 'white',
       height: 95,
-      padding: '0 30px',
+      padding: '0 20px',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
     label: {
@@ -24,44 +23,33 @@ const StyledButton = withStyles({
     root: {
       background: 'linear-gradient(45deg, #388e3c 30%, #388e3c 90%)',
       borderRadius: 3,
+      width:135,
       border: 0,
       color: 'white',
       height: 95,
-      padding: '0 30px',
+      padding: '0 10px',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
     label: {
       textTransform: 'capitalize',
     },
   })(Button);
-  const BlueButton = withStyles({
-    root: {
-      background: 'linear-gradient(45deg, rgb(100, 141, 174) 30%, rgb(100, 150, 174) 90%)',
-      borderRadius: 3,
-      border: 0,
-      color: 'white',
-      height: 95,
-      padding: '0 30px',
-      boxShadow: '0 3px 3px 2px grey',
-    },
-    label: {
-      textTransform: 'capitalize',
-    },
-  })(Button);
+  
  
 const SwipeButtons = () => {
     return (
         <div className="swipeButtons">
                  <StyledButton><CloseIcon fontSize="large" />IGNORE</StyledButton>
-            <Button className="swipeButtons__call" variant="contained" color="default">
+            <Button className="swipeButtons__call" variant="contained" color="primary">
                  <CallIcon fontSize="large" /> CALL NOW
             </Button>
-            <BlueButton><FavoriteIcon fontSize="large" />WAIT LIST</BlueButton>
             <StyleButton><CheckIcon fontSize="large" />HIRE NOW</StyleButton>
         </div>
     );
 };
 
 export default SwipeButtons;
+
+
 
 
