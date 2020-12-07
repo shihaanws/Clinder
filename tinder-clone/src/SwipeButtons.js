6fabc409cb4456ec5bca1bf4a56358c73b297d42
Shihaan <42083808+theneoterik@users.noreply.github.com>
2020-12-07 21:36:34 +0530

    Update SwipeButtons.js

--- tinder-clone/src/SwipeButtons.js
+++ tinder-clone/src/SwipeButtons.js
@@ -8,10 +8,12 @@ import { withStyles } from '@material-ui/core/styles';
 const StyledButton = withStyles({
     root: {
       background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-      borderRadius: 3,
+      borderRadius: 15,
       border: 0,
       color: 'white',
-      height: 95,
+      height: 80,
+      marginTop: 25,
+      width:100,
       padding: '0 20px',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
@@ -22,11 +24,30 @@ const StyledButton = withStyles({
   const StyleButton = withStyles({
     root: {
       background: 'linear-gradient(45deg, #388e3c 30%, #388e3c 90%)',
-      borderRadius: 3,
-      width:135,
+      borderRadius: 15,
+      width:100,
       border: 0,
       color: 'white',
-      height: 95,
+      height: 80,
+      marginTop: 25,
+      width:100,
+      padding: '0 10px',
+      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    },
+    label: {
+      textTransform: 'capitalize',
+    },
+  })(Button);
+  const CallButton = withStyles({
+    root: {
+      background: 'linear-gradient(45deg, #008CBA 30%, #008CBA 90%)',
+      borderRadius: 15,
+      width:100,
+      border: 0,
+      color: 'white',
+      height: 80,
+      marginTop: 25,
+      width:100,
       padding: '0 10px',
       boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
     },
@@ -40,15 +61,18 @@ const SwipeButtons = () => {
     return (
         <div className="swipeButtons">
                  <StyledButton><CloseIcon fontSize="large" />IGNORE</StyledButton>
-            <Button className="swipeButtons__call" variant="contained" color="primary">
-                 <CallIcon fontSize="large" /> CALL NOW
-            </Button>
+  <CallButton><CallIcon fontSize="large" />CALL NOW</CallButton>
+
+            
             <StyleButton><CheckIcon fontSize="large" />HIRE NOW</StyleButton>
         </div>
     );
 };
 
 export default SwipeButtons;
+
+
+
 
 
 
